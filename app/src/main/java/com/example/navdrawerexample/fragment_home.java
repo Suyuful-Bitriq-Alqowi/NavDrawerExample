c066589e7975ff7b5145177709ae358633227653
Suyuful-Bitriq-Alqowi <bitriqsteam@gmail.com>
2024-04-03 12:59:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/navdrawerexample/fragment_home.java
@@ -0,0 +1,27 @@
+package com.example.navdrawerexample;
+
+// HomeFragment.java
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.navdrawerexample.R;
+
+public class fragment_home extends Fragment {
+
+    public fragment_home() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
